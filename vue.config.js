afe14659f280ce41fc00b7791e4aad9ff647784a
drinkeewu <773119690@qq.com>
2021-05-22 12:03:28 +0800

    fix:修复打包路径

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
+const isDev = process.env.NODE_ENV === 'development'
 module.exports = {
   devServer: {
     overlay: false
-  }
+  },
+  publicPath: isDev
+    ? '/'
+    : '/trade-tools/dist/'
 }
