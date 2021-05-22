cc2270607c201c5978fa64cfe017e7bc5ca2365a
drinkeewu <773119690@qq.com>
2021-05-22 11:30:36 +0000

    auto deploy

---
+++ README.md
@@ -0,0 +1,37 @@
+# trade-tools
+> Some tools collects
+
+<p>
+   <a href="https://travis-ci.org/github/drinkeewu/drinkeewu.github.io">
+    <img src="https://travis-ci.org/drinkeewu/drinkeewu.github.io.svg?branch=master" >
+  </a>
+</p>
+
+[Link](https://drinkeewu.github.io/trade-tools/#/)
+
+## Feature
+- 回报计算
+- PWA Support
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ precache-manifest.2d1820917c95089fa91c2bd7a631dd3e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0994d5d43327e89312db",
+    "url": "/trade-tools/css/about.67326f0d.css"
+  },
+  {
+    "revision": "025a17ba31352a8b72fc",
+    "url": "/trade-tools/css/app.e0ccd625.css"
+  },
+  {
+    "revision": "6a6f33afb12d7262cc2a988c6b543c34",
+    "url": "/trade-tools/index.html"
+  },
+  {
+    "revision": "0994d5d43327e89312db",
+    "url": "/trade-tools/js/about.06624484.js"
+  },
+  {
+    "revision": "025a17ba31352a8b72fc",
+    "url": "/trade-tools/js/app.7daab695.js"
+  },
+  {
+    "revision": "8679d62143e1c9f04849",
+    "url": "/trade-tools/js/chunk-vendors.20a8b95c.js"
+  },
+  {
+    "revision": "b0fc708d939df36d22c5d7ef4fa83f1a",
+    "url": "/trade-tools/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/trade-tools/robots.txt"
+  }
+]);
